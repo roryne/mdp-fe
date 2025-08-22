fc44d0381a8e64bd803d720246acbef3595db6af
Ryne O <r.m.overfield@gmail.com>
2025-08-22 00:05:59 -0500

    use GH annotations for test reporting

--- playwright-ct.config.ts
+++ playwright-ct.config.ts
@@ -11,7 +11,7 @@ export default defineConfig({
   fullyParallel: true,
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 2 : 0,
-  reporter: 'dot',
+  reporter: process.env.CI ? 'github' : 'dot',
   snapshotPathTemplate: '{testDir}/{testFilePath}-snapshots/{testName}{ext}',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
