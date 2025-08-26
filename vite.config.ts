0dff910ab0ccdd7954649067cdcf6007efcf2e36
Ryne O <r.m.overfield@gmail.com>
2025-08-26 18:57:40 -0500

    chore(multienvironment): fix files necessary for provisioning rc env

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,5 @@
 import react from '@vitejs/plugin-react'
-import path from 'node:path'
+import * as path from 'node:path'
 import { fileURLToPath } from 'node:url'
 import { defineConfig } from 'vite'
 
