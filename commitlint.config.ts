ad86abbbba05dd2946fd08034d2067ab9e6b9940
Ryne O <r.m.overfield@gmail.com>
2025-09-01 17:42:38 -0500

    wip: change commitlint to allow more inclusive wip msg

--- commitlint.config.ts
+++ commitlint.config.ts
@@ -3,7 +3,7 @@ import { type UserConfig } from '@commitlint/types'
 const Configuration: UserConfig = {
   defaultIgnores: true,
   extends: ['@commitlint/config-conventional'],
-  ignores: [(message) => message.startsWith('wip:')]
+  ignores: [(message) => message.startsWith('wip')]
 }
 
 export default Configuration
